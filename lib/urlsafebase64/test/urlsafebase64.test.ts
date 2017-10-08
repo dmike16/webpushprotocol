8aad37048c02fd9afa69877bf19235a5f8fe392b
dmike16 <cipmiky@gmail.com>
2017-10-08 22:56:28 +0200

    [ADD] Add laste test for urlbase64
    All tests are completed for urlbase64.
      TESING:
          1. Encode function change the mode
          2. Decode function change the mode
          3. Update function fill internal buffer
          4. Correct encoding in urlbase64 [using update and digest]
          5. Correct decoding from urlbase64 [using stream event]
    
    ===================================
        [OP]:
        -* ADD -> Add new stuff
        -* DEL -> Delete stuff
        -* MOD -> Modifies stuff
        -* FIX -> Fix Bugs
        -* MER -> Merge Branch
        -* MEF -> Merge fixing conflit
    
    ===================================

--- lib/urlsafebase64/test/urlsafebase64.test.ts
+++ lib/urlsafebase64/test/urlsafebase64.test.ts
@@ -1,7 +1,7 @@
 import { Urlbase64Mode, UrlsafeBase64 } from '../src/Urlbase64';
-import { expect } from 'chai';
 
 import createUrlsafeBase64 from '../src/Urlbase64';
+import { expect } from 'chai';
 
 describe('UrlsafeBase64', function() {
   let testInstance: UrlsafeBase64;
@@ -12,7 +12,7 @@ describe('UrlsafeBase64', function() {
 
   describe('#encode()', function() {
     it('The mode should be encoding', function() {
-      return testingPromise(()=>{
+      return testingPromise(() => {
         expect(testInstance.encode().mode).to.be.equals(Urlbase64Mode.ENCODE);
       })
     });
@@ -20,28 +20,52 @@ describe('UrlsafeBase64', function() {
 
   describe('#decode()', function() {
     it('The mode should be decoding', function() {
-      return testingPromise(()=>{
+      return testingPromise(() => {
         expect(testInstance.decode().mode).to.be.equals(Urlbase64Mode.DECODE);
       })
     });
   });
 
   describe('#update()', function() {
-    it('Should fill the internal buffer');
+    it('Should fill the internal buffer', function() {
+      return testingPromise(() => {
+        let s = 'ABCDEFG';
+        testInstance.update(s)
+        expect(testInstance.digest()).to.have.lengthOf.above(1);
+      })
+    });
   });
 
   describe('#digest()', function() {
-    it('Should encode in url base 64 the internal buffer');
-    it('Should decode the internal buffer');
+    let message: string;
+    let urlbase64_test :string;
+      beforeEach(function() {
+        message = '+ Hello urlsafebase64 +';
+        urlbase64_test = Buffer.from(message).toString('base64')
+          .replace(/\+/g, '-')
+          .replace(/\//g, '_')
+          .replace(/=+$/, '');
+      });
+    it('Should encode in url base 64 the internal buffer', function() {
+      testInstance.update(message);
+      expect(testInstance.digest()).to.be.equals(urlbase64_test);
+    });
+    it('Should decode the internal buffer',function(){
+      testInstance.on('data',(chunk)=>{
+        expect(chunk.toString()).to.be.equals(message);
+      });
+      testInstance.write(urlbase64_test);
+      testInstance.decode().end();
+    });
   });
 });
 
-function testingPromise(test:(arg?:void)=>void){
-  return new Promise<void>((resolve,reject)=>{
-    try{
+function testingPromise(test: (arg?: void) => void) {
+  return new Promise<void>((resolve, reject) => {
+    try {
       test();
       resolve();
-    }catch(err){
+    } catch (err) {
       reject(err);
     }
   })
