04f9a7fc3c47e7c6c16e382c03e517204cc8637b
dmike16 <cipmiky@gmail.com>
2018-09-23 00:28:16 +0200

    refactor(package): 1/3 refactor project structure
    Now all the sub project are in packages dir
    The build process is under developement

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": "google",
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "rules": {
+    "max-len": ["error", { "code": 100, "tabWidth": 2 }],
+    "indent": ["error", 2]
+  }
+};
